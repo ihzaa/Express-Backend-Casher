2c7f8a3c090604c00ea25ee5cb13669a64af9908
ihza0 <ihzaahmad0@gmail.com>
2023-02-19 12:50:58 +0700

    44 connect to mongodb

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 import express from "express";
-import logger from "morgan";
 import dotenv from "dotenv";
-
+import mongoose from "mongoose";
 import indexRouter from "./routes/index.js";
 
 const env = dotenv.config().parsed;
 var app = express();
 
-app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/", indexRouter);
+
+mongoose.set("strictQuery", false);
+mongoose.connect(
+  `${env.MONGODB_URI}${env.MONGODB_HOST}:${env.MONGODB_PORT}`,
+  {
+    dbName: env.MONGODB_DB_NAME,
+  }
+);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function () {
+  console.log("connected to mongodb");
+});
 
 app.listen(env.APP_PORT, () => {
   console.log("Server is running in port: " + env.APP_PORT);
