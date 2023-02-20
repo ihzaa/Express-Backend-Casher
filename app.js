8225c4f596c5e6a25858ce6954ff15c7d407ec5c
ihza0 <ihzaahmad0@gmail.com>
2023-02-21 00:05:11 +0700

    47 Controllers Category Store Data

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import indexRouter from "./routes/index.js";
 
+import {store} from "./controllers/CategoryController.js";
+
 const env = dotenv.config().parsed;
 var app = express();
 
@@ -10,14 +12,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/", indexRouter);
+app.post("/categories", store);
 
 mongoose.set("strictQuery", false);
-mongoose.connect(
-  `${env.MONGODB_URI}${env.MONGODB_HOST}:${env.MONGODB_PORT}`,
-  {
-    dbName: env.MONGODB_DB_NAME,
-  }
-);
+mongoose.connect(`${env.MONGODB_URI}${env.MONGODB_HOST}:${env.MONGODB_PORT}`, {
+  dbName: env.MONGODB_DB_NAME,
+});
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
 db.once("open", function () {
---
+++ controllers/CategoryController.js
@@ -0,0 +1,25 @@
+import category from "../models/Category.js";
+
+const store = async (req, res) => {
+  try {
+    const { title } = req.body;
+    const newCategory = new category({
+      title,
+    });
+    const Category = await newCategory.save();
+    if (!Category) {
+      throw { code: 500, message: "Store Category Failed!" };
+    }
+    return res.status(200).json({
+      status: true,
+      category: Category,
+    });
+  } catch (err) {
+    return res.status(err.code).json({
+      status: false,
+      message: err.message,
+    });
+  }
+};
+
+export {store}
